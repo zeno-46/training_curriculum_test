5da9b4f99d3b1f1d3f173af7c43f9b54ae9141be
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-05 21:18:53 +0900

    命名規則の変更

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,12 +3,12 @@ class CalendarsController < ApplicationController
   # １週間のカレンダーと予定が表示されるページ
   def index
     get_Week
-    @plan = Plan.new
+    @plan = plan.new
   end
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
+    plan.create(plan_params)
     redirect_to action: :index
   end
 
@@ -22,19 +22,19 @@ class CalendarsController < ApplicationController
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
-    @todays_date = Date.today
+    @todays_date = date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
 
-    plans = Plan.where(date: @todays_date..@todays_date + 6)
+    plans = plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month:  '(@todays_date + x).month', date: ' (@todays_date+x).day', plans: 'today_plans'}
+      days = { month:  (@todays_date + x).month, date: (@todays_date+x).day', plans: 'today_plans}
       @week_days.push(days)
     end
 
