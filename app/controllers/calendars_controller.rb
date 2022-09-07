4e968d1707e670e76ff5025faa62e7d4ae1eb99f
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-07 10:37:57 +0900

    ようやく曜日表示させられました

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -41,9 +41,8 @@ class CalendarsController < ApplicationController
         wday_num = wday_num -7
       end
 
-      days = { month:  (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, :wday =>  (@todays_date + x).day}
+      days = { month:  (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[Date.today.wday]}
 
-      
       @week_days.push(days)
     end
 
