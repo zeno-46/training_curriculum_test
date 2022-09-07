7d1089798af70d7fc1ed3d377d58a24c6ed022cf
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-07 14:40:56 +0900

    テスト

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -37,7 +37,7 @@ class CalendarsController < ApplicationController
 
 
 
-      wday_num = Date.today.wday + x
+       wday_num = Date.today.wday + x
 
       if wday_num >= 7
         wday_num = wday_num -7
