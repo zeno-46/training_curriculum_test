0c9bcc216bebb0d123883c8d753bbc9b62f7b1c5
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-07 14:32:31 +0900

    曜日の表示

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,12 +36,12 @@ class CalendarsController < ApplicationController
       end
 
 
-      wday_num = Date.today.wday
+      wday_num = Date.today.wday + x
       if wday_num >= 7
         wday_num = wday_num -7
       end
 
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num + x]}
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num ]}
 
       @week_days.push(days)
     end
